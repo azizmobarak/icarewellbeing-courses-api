e208dc303e6fad25f6eca53874057d5354c3f1cf
azizmobarak <mobarakaziz9@gmail.com>
2023-04-15 22:13:24 +0000

    origin all

--- server.ts
+++ server.ts
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000","http://localhost:3000/*"],
+  origin: ["*"],
   credentials: true,
   methods: "GET,PUT,PATCH,POST,DELETE,UPDATE",
 }))
