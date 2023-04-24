f643b39ab95cc7d43066edfa097ca48e81e21e0d
azizmobarak <mobarakaziz9@gmail.com>
2023-04-24 22:56:19 +0000

    cors

--- index.ts
+++ index.ts
@@ -13,6 +13,26 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
+
+app.use(function (_req: any, res: any, next: any) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://courses.billivance.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 const domainsFromEnv = process.env.CORS_DOMAINS || '*'
 
 const whitelist = domainsFromEnv.split(',').map((item) => item.trim())
