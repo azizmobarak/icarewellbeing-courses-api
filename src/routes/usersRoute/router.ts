9c744f2ae94032e6c841dbcfbc6f98a6c6a9a01d
azizmobarak <mobarakaziz9@gmail.com>
2023-05-01 02:20:20 +0100

    user crud

--- src/routes/usersRoute/router.ts
+++ src/routes/usersRoute/router.ts
@@ -10,6 +10,8 @@ import {
 } from '../../middlewares/userMiddlwares/validationMiddlwares'
 import { resetPassword } from '../../controllers/users/resetPassword'
 import { requestresetPassword } from '../../controllers/users/requestResetPassword'
+import { deleteUser } from '../../controllers/users/deleteUser'
+import { isAuth, isSuperAdmin } from '../../middlewares/authMiddleware/middleware'
 
 router.route('/register').post(validateUser, addNewUser)
 // Put accepts password and token
@@ -17,5 +19,6 @@ router.route('/resetPassword').put(validateResetPassword, resetPassword)
 router
     .route('/requestPassword')
     .post(validateRequestRestPassword, requestresetPassword)
+router.route('/user/delete').delete(isAuth,isSuperAdmin,deleteUser);
 
 module.exports = router
